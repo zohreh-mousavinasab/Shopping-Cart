ce1bca5338f583f7423970eca736e38a464e7402
zohreh-mousavinasab <mousavinasab.zohreh@gmail.com>
2021-08-31 11:25:45 +0430

    first

---
+++ products.js
@@ -0,0 +1,41 @@
+export const productsData = [
+    {
+      id: 1,
+      title: "queen panel bed",
+      price: 16.90,
+      imageUrl: "./images/1.jpg",
+    },
+    {
+      id: 2,
+      title: "La Resta Double Bed",
+      price: 15.99,
+      imageUrl: "./images/2.jpg",
+    },
+    {
+      id: 3,
+      title: "Bari Queen Bed",
+      price: 17.0,
+      imageUrl: "./images/3.jpg",
+    },
+    {
+      id: 4,
+      title: "Oakridge Queen Bed",
+      price: 21.32,
+      imageUrl: "./images/4.jpg",
+    },
+    {
+      id: 5,
+      title: "Coolmore King Bed",
+      price: 34.89,
+      imageUrl: "./images/5.jpg",
+    },
+    {
+      id: 6,
+      title: "Bjorn Bed Frame",
+      price: 33.93,
+      imageUrl: "./images/6.jpg",
+    },
+    
+    
+  ];
+  
